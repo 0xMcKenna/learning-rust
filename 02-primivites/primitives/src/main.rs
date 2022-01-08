31c70cd7033f11814fd6614a2521918379b7ed16
0xMcKenna <0xMcKenna@protonmail.com>
2022-01-08 10:00:45 +0000

    02-primitives

---
+++ 02-primivites/primitives/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    
+    // Type annotated variables (define primitive)
+    let n1: i64 = 100;
+
+    let n2 = 7; // default i32
+
+    // Mutable variables
+    let mut mutable: bool = false;
+    mutable = true;
+
+    // Cannot change mutable variables type.
+    // mutable = 21;
+}
